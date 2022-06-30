daee94ea0d700da6c41e18b51c06e712c3471208
harut.manukyan.g@gmail.com <harut.manukyan.g@gmail.com>
2022-06-30 12:58:43 +0400

    Add project files.

---
+++ Exercises1/Exercises1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercises1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Ex.1 | Adds two first ones and multiplies them by a third one.
+
+            //Console.Write("Input 1 number = ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input 2 number = ");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input 3 number = ");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int result = (a + b) * c;
+            //Console.Write($"Result = {result}");
+            #endregion
+
+            #region Ex.2. | addition, substraction, multiplication, division
+
+            //Console.WriteLine("Input 1 number = ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input 2 number = ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int addition = num1 + num2;
+            //Console.Write($"(+) {addition},");
+            //int substraction = num1 - num2;
+            //Console.Write($"(-) {substraction},");
+            //int multiplication = num1 * num2;
+            //Console.Write($"(*) {multiplication},");
+
+            //if (num2 == 0)
+            //{
+            //    Console.WriteLine("You can't divide any number by 0!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"(/) {num1/num2}:");
+            //}
+
+            #endregion
+        }
+    }
+}
