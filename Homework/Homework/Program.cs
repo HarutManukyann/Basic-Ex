722785d3bba70bf5245b256f7a89072aee05f88a
harut.manukyan.g@gmail.com <harut.manukyan.g@gmail.com>
2022-07-06 04:37:43 +0400

    Ex. 31 .

--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -159,17 +159,187 @@ namespace Homework
             }
 
         }
+        static void EvenNum()
+        {
+            Console.WriteLine("Input Numbers any you want:");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("________________________________");
+
+            if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+
+        }
+        static void ArithmeticProg()
+        {
+            Console.WriteLine("Input Numbers any you want:");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("________________________________");
+
+            if (a - b == b - c || a - c == c - b || b - a == a - c ||
+                b - c == c - a || c - b == b - a || c - a == a - b)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+        static void GeometricProg()
+        {
+            Console.WriteLine("Input Numbers any you want:");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("________________________________");
+
+            //_________________________EMPTY
+        } //?
+
+        static void AcsendingOrder()
+        {
+            Console.WriteLine("Input Numbers any you want:");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("________________________________");
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine(a);
+                if (b < c)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+
+                }
+            }
+            else if (b < a && b < c)
+            {
+                Console.WriteLine(b);
+                if (a < c)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                }
+            }
+            else
+            {
+                Console.WriteLine(c);
+                if (b < a)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                }
+            }
+
+        }
+        static void DecreasingOrder()
+        {
+            Console.WriteLine("Input Numbers any you want:");
+            Console.Write("a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("________________________________");
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+                if (b > c)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(b);
+
+                }
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine(b);
+                if (a > c)
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(c);
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    Console.WriteLine(a);
+                }
+            }
+            else
+            {
+                Console.WriteLine(c);
+                if (b > a)
+                {
+                    Console.WriteLine(b);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
             //Given 3 different numbers a,b,c 
             //TheBiggest(); // Find the biggest of 3 numbers
             //TheSmallest(); // Find the smallest of 3 numbers
-            //EqualOne(); // If one of the given numbers  is equal 1. Output TRUE or FALSE
-            //EqualTwo(); // If two of the given numbers is equal 2. Output TRUE or FALSE
+            //EqualOne(); // If one of the given numbers  is equal 1. Output TRUE otherwise FALSE
+            //EqualTwo(); // If two of the given numbers is equal 2. Output TRUE otherwise FALSE
             //Triangle();  // Is it possible to get a triangle. If yes output 'y=1', else 'y=2'
-
-
-
+            //EvenNum();  //If one of the given numbers  is even. Output 1 otherwise 2
+            //ArithmeticProg(); // If these numbers form an arithmetic progression. Output TRUE otherwise FALSE
+            //?GeometricProg(); // If these numbers form an geometric progression. Output TRUE otherwise FALSE
+            //AcsendingOrder(); // Output sorted in acsending order <
+            //DecreasingOrder(); // Output sorted in decreasing order >
 
         }
     }
