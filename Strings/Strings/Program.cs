e72b23e1415c45b248684d9435c98c2c5887666d
harut.manukyan.g@gmail.com <harut.manukyan.g@gmail.com>
2022-07-07 16:24:47 +0400

    String exercises

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -6,29 +6,85 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            Console.Write("Input string: ");
-            string str = Console.ReadLine();
-            int max = 0;
-            char sym = ' ';
+            #region String max symbol
+            //Console.Write("Input string: ");
+            //string str = Console.ReadLine();
+            //int max = 0;
+            //char sym = ' ';
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    int count = 0;
+            //    for (int j = 0; j < str.Length; j++)
+            //    {
+            //        if (str[i] == str[j])
+            //        {
+            //            count++;
+            //        }
+
+            //    }
+            //    if (count > max)
+            //    {
+            //        max = count;
+            //        sym = str[i];
+            //    }
+
+            //}
+            //Console.WriteLine($"Max Count = {max} , symbol = {sym}");
+            #endregion
+            #region Ex. 151 
+            //Console.Write("How many numbers do you want to input: ");
+            //int numCount = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input dividing number");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input only numbers:");
+            //string[] str = new string[numCount];
+            //int indexNum = 0;
+            //int sum = 0;
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    str[i] = Console.ReadLine();
+            //    indexNum = Convert.ToInt32(str[i]);
+            //    if (indexNum%n == 0)
+            //    {
+            //        sum += indexNum;
+            //    }
+            //}
+            //Console.WriteLine("_____________________________");
+            //Console.WriteLine($"{sum}");
+            #endregion
+
+            #region Ex. 152
+            Console.Write("How many numbers do you want to input: ");
+            int numCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input dividing number. it can not be zero: ");
+            int n;
+            do
+            {
+                Console.Write("Divide number = ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+            } while (n <= 0);
+            Console.WriteLine("Input only numbers:");
+            string[] str = new string[numCount];
+            int indexNum = 0;
+            int result = 1;
             for (int i = 0; i < str.Length; i++)
             {
-                int count = 0;
-                for (int j = 0; j < str.Length; j++)
+                str[i] = Console.ReadLine();
+                indexNum = Convert.ToInt32(str[i]);
+                if (indexNum % n == 0)
                 {
-                    if (str[i] == str[j])
-                    {
-                        count++;
-                    }
-
+                    result *= indexNum;
                 }
-                if (count > max)
-                {
-                    max = count;
-                    sym = str[i];
-                }
-
             }
-            Console.WriteLine($"Max Count = {max} , symbol = {sym}");
+            Console.WriteLine("_____________________________");
+            Console.WriteLine($"{result}");
+            #endregion
+
+
+
+
 
 
         }
