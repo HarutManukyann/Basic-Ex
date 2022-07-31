e65dc29af74b23e7386f8eadcb8b75c4dfbf9993
harut.manukyan.g@gmail.com <harut.manukyan.g@gmail.com>
2022-07-31 13:12:42 +0400

    Zaviknoc

---
+++ OOP/OOP/Burger.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Burger : IFoodInclude
+    {
+
+        public string size;
+        public bool isDiet;
+        public string includingFoods;
+        public int kolori;
+        public static DateTime time = DateTime.Now;
+
+        public Burger()
+        {
+
+        }
+        public Burger(string size, bool isDiet, string includingFoods,int kolori)
+        {
+
+            this.size = size;
+            this.isDiet = isDiet;
+            this.includingFoods = includingFoods;
+            this.kolori = kolori;
+        }
+
+        public void Composition()
+        {
+            if (isDiet)
+            {
+                Console.WriteLine("Diet burger");
+            }
+            else
+            {
+                Console.WriteLine("NOT Diet");
+            }
+            Console.WriteLine($"Size is: {size}.");
+            Console.WriteLine($"Burger contains: {includingFoods}");
+            Console.WriteLine($"Koloris in burger:{kolori }");
+
+            IFoodInclude obj = new Burger();                  //connect interface method, which have a body
+            Console.WriteLine($"Price: {obj.Price(includingFoods,time)}");
+
+        }
+    }
+}
---
+++ OOP/OOP/IFoodInclude.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public interface IFoodInclude
+    {
+        public static int price { get; set; }
+        public static string size { get; set; }
+        public static bool isDiet { get; set; }
+        public static int kalori { get; set; }
+        public static string includingFoods { get; set; }
+        public static DateTime time = new DateTime();
+
+
+        public void Composition();
+        public int Price(string includingFoods, DateTime time)
+        {
+            int price = 850;
+            if (includingFoods.ToLower().Contains("cheese"))
+            {
+                Console.WriteLine("Cheese: +500");
+                price += 500;
+            }
+            if (includingFoods.ToLower().Contains("meat"))
+            {
+                Console.WriteLine("Meat: +1000");
+                price += 1000;
+            }
+            time = DateTime.Now;
+            Console.WriteLine($"Burger's Date :  {time}");
+            return price;
+
+
+        }
+    }
+}
